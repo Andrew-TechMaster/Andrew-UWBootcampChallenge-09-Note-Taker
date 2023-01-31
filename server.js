7fa0c3943f41657a45305807b99c66b586ce15aa
Andrew Chou <andrewchou1996@gmail.com>
2023-01-30 20:27:20 -0800

    Initial setup for the home page

---
+++ server.js
@@ -0,0 +1,22 @@
+//  Include External Modules 
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+// const api = require('./routes/index.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
